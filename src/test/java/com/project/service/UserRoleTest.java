db46b5edb017f9824ddf96c486c6ab2d999246d5
Jose Mª Villar <x271091@gruposantander.es>
2022-03-15 18:28:20 +0100

    Issue. JUnit4 assertions are not correctly updated to JUnit5.

--- src/test/java/com/project/service/UserRoleTest.java
+++ src/test/java/com/project/service/UserRoleTest.java
@@ -6,12 +6,16 @@ import com.project.model.UserRole;
 import com.project.model.id.UserRoleKey;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.function.ThrowingRunnable;
 import org.mockito.Mockito;
 
 import java.sql.Timestamp;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 
 public class UserRoleTest {
 
@@ -51,9 +55,21 @@ public class UserRoleTest {
     }
 
     @Test
-    void findAllAssociations() {
+    void assertionsTest() {
         assertNotNull(userRole.getId());
         assertEquals(role, userRole.getRole());
         assertEquals(user, userRole.getUser());
+
+        Exception exception = assertThrows(NumberFormatException.class, new ThrowingRunnable() {
+            public void run() throws Throwable {
+                Integer.parseInt("1a");
+            }
+        });
+
+        String expectedMessage = "For input string";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+        assertThat(actualMessage).isEqualTo(expectedMessage);
     }
 }
